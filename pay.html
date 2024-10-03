8d29422c3c19922c9b19d8200ca758a70098a953
sarvusahitya <sarvusahitya@gmail.com>
2024-10-03 22:11:32 +0530

    update

---
+++ pay.html
@@ -0,0 +1,3 @@
+<a href="upi://pay?pa=UPIID@oksbi&amp;pn=FNAME SNAME K&amp;cu=INR" class="upi-pay1">Pay Now 1</a>
+
+<a href="upi://pay?pa=UPIID@oksbi&amp;pn=JOHN BRITAS AK &amp;cu=INR" class="upi-pay1">Pay Now !</a>
---
+++ resources/views/pay.blade.php
@@ -0,0 +1,7 @@
+<?php
+
+?>
+
+<a href="upi://pay?pa=UPIID@oksbi&amp;pn=FNAME SNAME K&amp;cu=INR" class="upi-pay1">Pay Now 1</a>
+
+<a href="upi://pay?pa=UPIID@oksbi&amp;pn=JOHN BRITAS AK &amp;cu=INR" class="upi-pay1">Pay Now !</a>
