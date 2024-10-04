9d244b757321fb0786d195605515d6ba6977f785
sarvusahitya <sarvusahitya@gmail.com>
2024-10-04 10:57:01 +0530

    update

--- resources/views/pay.blade.php
+++ resources/views/pay.blade.php
@@ -5,3 +5,9 @@
     <a href="upi://pay?pa=UPIID@oksbi&amp;pn=FNAME SNAME K&amp;cu=INR" class="upi-pay1">Pay Now test</a></br>
 
     <a href="upi://pay?pa=UPIID@oksbi&amp;pn=JOHN BRITAS AK &amp;cu=INR" class="upi-pay1">Pay Now test2</a>
+
+
+
+    <a href="upi://pay?pa=trushitgadhavi99134-1@okaxix&amp;pn=JOHN BRITAS AK&amp;mc=1234&amp;tid=TxnID12345&amp;tr=1234567890&amp;tn=Payment+for+Services&amp;am=1000.00&amp;cu=INR" class="upi-pay1">
+        Pay Now
+    </a>
